cd511b61785f78780a1591923eb3f8d88382ae59
Jon Turney <jon.turney@dronecode.org.uk>
2022-04-14 15:33:18 +0100

    Initial commit

---
+++ ctm2git.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import re
+import shutil
+import subprocess
+import sys
+import urllib.request
+import xtarfile
+
+CACHE_DIR = '/tmp/ctm2git/cache'
+ARCHIVE_EXTS = [
+    '.tar.gz', '.tgz',
+    '.tar.bz2', '.tbz',
+    '.tar.lzma',
+    '.tar.xz', '.txz',
+    '.tar.zst',
+]
+DEFAULT_AUTHOR = 'unknown <unknown@unknown.invalid>'
+
+
+def url_retrieve_cached(u):
+    cache_fn = os.path.join(CACHE_DIR, u.replace('http://', '').replace(os.path.sep, '_'))
+
+    if not os.path.isfile(cache_fn):
+        (filename, headers) = urllib.request.urlretrieve(u, cache_fn)
+        print('fetching %s' % u, file=sys.stderr)
+    else:
+        filename = cache_fn
+        # print('%s from cache' % filename, file=sys.stderr)
+
+    return filename
+
+
+class source:
+    def __init__(self, url, author):
+        self.url = url
+        self.author = author
+
+
+def ctm_to_sourcelist(args):
+    if args.arch == 'x86':
+        index_url = "http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/circa/index.html"
+    else:
+        index_url = "http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/circa/64bit/index.html"
+
+    # read index, build list of setup.uni URLs
+    urls = []
+    html = urllib.request.urlopen(index_url).read().decode()
+    for l in html.splitlines():
+        m = re.search('<td>(http.*)</td>', l)
+        if m:
+            urls.append(m.group(1) + '/setup.ini')
+
+    # for each setup.ini URL, fetch it and parse details for selected package
+    sources = {}
+    for u in urls:
+        circa = re.search('^(.*)/setup.ini$', u).group(1)
+        filename = url_retrieve_cached(u)
+
+        # parse it
+        with open(filename, errors='ignore') as f:
+            v, s = parse_setup_ini(f.read(), args.package[0])
+            if v:
+                # because we circas are ordered newest to oldest, this records
+                # the oldest circa to contain the version
+                sources[v] = circa + '/' + s
+
+    # show versions and sources
+    for v in reversed(sources):
+        print(v, sources[v], DEFAULT_AUTHOR)
+
+
+def sourcelist_to_repo(args):
+    # read from provided filename
+    sources = {}
+    with open(args.sourcelist[0]) as f:
+        for l in f.readlines():
+            l = l.strip()
+            (v, url, author) = l.split(sep=None, maxsplit=2)
+            if author == DEFAULT_AUTHOR:
+                print('Unknown author still in sourcelist', file=sys.stderr)
+                exit(1)
+            sources[v] = source(url, author)
+
+    # abort if working directory is already a git repo
+    if os.path.exists('.git'):
+        print('Working directory is already a git repo', file=sys.stderr)
+        exit(1)
+
+    # abort if working directory isn't empty
+    if len(os.listdir('.')) != 0:
+        print("Working directory isn't empty", file=sys.stderr)
+        exit(1)
+
+    subprocess.check_call(['git', 'init'])
+
+    # for each unique source...
+    for v in sources:
+        # fetch it
+        url = sources[v].url
+        author = sources[v].author
+        filename = url_retrieve_cached(url)
+
+        # clean working directory
+        with os.scandir('.') as entries:
+            for entry in entries:
+                if entry.path == './.git':
+                    continue
+                elif entry.is_dir() and not entry.is_symlink():
+                    shutil.rmtree(entry.path)
+                else:
+                    os.remove(entry.path)
+
+        # Look inside source package archive to see if filenames start with a
+        # directory with a name ending with '.src' (as current versions of
+        # cygport make) ...
+        with xtarfile.open(filename, mode='r') as archive:
+            strip = any(re.match(r'[^/]*\.src/', f) for f in archive.getnames())
+
+        # ... if so, use --strip-components to trim that
+        extra_args = ''
+        if strip:
+            extra_args = '--strip-components=1'
+
+        # unpack it
+        subprocess.check_call(['tar', '-x', extra_args, '-f', filename])
+
+        # remove upstream tarball(s)
+        with os.scandir('.') as entries:
+            for entry in entries:
+                if any(entry.path.endswith(ext) for ext in ARCHIVE_EXTS):
+                    os.remove(entry.path)
+
+        # create a git commit
+        subprocess.check_call(['git', 'add', '--all', '-f', '.'])
+        circa = re.search(r'circa/([\d/]*)/', url).group(1)
+        date = circa + ' UTC'
+
+        env = os.environ.copy()
+        env['GIT_COMMITTER_NAME'] = re.search(r'^(.*) <', author).group(1)
+        env['GIT_COMMITTER_EMAIL'] = re.search(r'<(.*)>', author).group(1)
+        env['GIT_COMMITTER_DATE'] = date
+
+        message = '%s %s\n\nctm2git-circa: %s' % (args.package[0], v, circa)
+        subprocess.check_call(['git', 'commit', '--author', author, '--date=%s' % date, '-m', message], env=env)
+
+
+def parse_setup_ini(contents, package):
+    for l in contents.splitlines():
+        if l.startswith('@'):
+            p = l[2:]
+        elif l.startswith('version:'):
+            v = l[9:]
+        elif l.startswith('source:'):
+            s = l[8:].split()[0]
+            # this will stop on the source: line for the first version: line
+            # (the 'current' version) for the specified package
+            if p == package:
+                return v, s
+
+    return None, None
+
+
+parser = argparse.ArgumentParser(description='Make a git repository from CTM package history')
+parser.add_argument('package', action='store', nargs=1)
+parser.add_argument('--arch', action='store', required=True, choices=['x86', 'x86_64'])
+parser.add_argument('--sourcelist', action='store', nargs=1, help='sourcelist from a previous run')
+(args) = parser.parse_args()
+if not args.sourcelist:
+    ctm_to_sourcelist(args)
+else:
+    sourcelist_to_repo(args)
